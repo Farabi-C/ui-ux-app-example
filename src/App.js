98d83efdd9afed21430589d947fe325a27449aaf
Farabi Choudhury <farabi@Farabis-MacBook-Air.local>
2023-02-08 16:21:22 -0500

    my-app-ui-ux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  function handleClick(){
+    alert("Button was clicked");
+  }
+  return (
+    <button onClick={handleClick}>
+      I'm a button
+    </button>
+  );
+}
+// In JSX, the function must return a single element 
+//
 function App() {
+  let her = "She";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><span style={{color:"purple"}}>{her}</span> fractured my solitude</h1>
+      <MyButton/> {/* Unnecessary to add the starting tag when nesting components */}
+      <h2>I feel I can give you everything without giving myself away</h2>
     </div>
   );
 }
